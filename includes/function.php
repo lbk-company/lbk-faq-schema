8a821b4b536f8705352bcd380c1c9954d9a39997
Brikibui17 <brikibui17@gmail.com>
2021-03-26 00:18:47 +0700

    Update function.php

--- includes/function.php
+++ includes/function.php
@@ -4,7 +4,7 @@ if ( !function_exists('lbk_sanitize_faqs_field') ) {
     function lbk_sanitize_faqs_field( $meta_value ) {
         foreach ( (array) $meta_value as $key => $value ) {
             if ( is_array($value) ) {
-                $meta_value[$key] = sanitize_faqs_field( $value );
+                $meta_value[$key] = lbk_sanitize_faqs_field( $value );
             }
             else {
                 $meta_value[$key] = sanitize_text_field( $value );
